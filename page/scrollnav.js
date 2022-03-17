9550ee1af2eae7b0e267039f8dfa488d3b4694e9
HiKE-yt <68474172+HiKE-yt@users.noreply.github.com>
2022-03-17 18:58:17 +0100

    -

--- page/scrollnav.js
+++ page/scrollnav.js
@@ -1,12 +1,18 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// When the user scrolls down 80px from the top of the document, resize the navbar-right's padding and the logo's font size
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.padding = "30px 10px";
-        document.getElementById("logo").style.fontSize = "25px";
+        document.getElementById("navbar-right").style.paddingTop = "15px";
+        document.getElementById("navbar-right").style.paddingRight = "20px";
+        document.getElementById("navbar-right").style.paddingBottom = "15px";
+        document.getElementById("navbar-right").style.paddingLeft = "10px";
+        document.getElementById("navbar-right").style.height = "25px";
     } else {
-        document.getElementById("navbar").style.padding = "80px 10px";
-        document.getElementById("logo").style.fontSize = "35px";
+        document.getElementById("navbar-right").style.paddingTop = "35px";
+        document.getElementById("navbar-right").style.paddingRight = "20px";
+        document.getElementById("navbar-right").style.paddingBottom = "15px";
+        document.getElementById("navbar-right").style.paddingLeft = "10px";
+        document.getElementById("navbar-right").style.height = "35px";
     }
 }
